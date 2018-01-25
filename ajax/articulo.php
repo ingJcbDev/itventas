320906c01cf7551cfdd2fc392435e1f355fb530b
ingJcbDev <ing.jcb.dev@gmail.com>
2018-01-24 22:16:12 -0500

    030 Subir y guardar imagen en nuestro proyecto mostrar las imágenes en la vista

--- ajax/articulo.php
+++ ajax/articulo.php
@@ -12,6 +12,19 @@ $descripcion = isset($_POST["descripcion"]) ? limpiarCadena($_POST["descripcion"
 $imagen = isset($_POST["imagen"]) ? limpiarCadena($_POST["imagen"]) :"";
 
 switch ($_GET["op"]) {
+
+	if(!file_exists($_FILES['imagen']['tmp_name']) || !is_uploaded_file($_FILES['imagen']['tmp_name']))
+	{
+		$imagen="";
+	}else{
+		$ext=explode(".", $_FILES["imagen"]["name"]);
+		if($_FILES['imagen']['type'] == "image/jpg" || $_FILES['imagen']['type'] == "image/jpeg" || $_FILES['imagen']['type'] == "image/png")
+		{
+			$imagen= round(microtime(true)) . '.' . end($ext);
+			move_uploaded_file($_FILES["imagen"]["tmp_name"], "../files/articulos/" . $imagen);
+		}
+	}
+
 	case 'guardaryeditar':
 			if(empty($idarticulo)){
 				$rspta = $articulo->insertar($idcategoria, $codigo, $nombre, $stock, $descripcion, $imagen);
@@ -52,7 +65,7 @@ switch ($_GET["op"]) {
 						"2"=>$reg->categoria,
 						"3"=>$reg->codigo,
 						"4"=>$reg->stock,
-						"5"=>$reg->imagen,
+						"5"=>"<img src='../files/articulos/".$reg->imagen."' height='50px' width='50px'>",
 						"6"=>($reg->condicion) ? '<span class="label bg-green">Activado</span>' : '<span class="label bg-red">Desactivado</span>' 
 					);
 				}
